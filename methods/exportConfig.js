6d099168e2f3ca557de0dc92b40170b6a4234bdd
Barış Demirci <barbarbar338@picass.co>
2020-07-08 16:08:29 +0000

    fixed importConfig

--- methods/exportConfig.js
+++ methods/exportConfig.js
@@ -1,4 +1,5 @@
 module.exports = (self, file) => {
     self.fileManager.checkFile(file);
     self.fileManager.writeFile(file, self.config);
+    return self.config;
 };
--- methods/importConfig.js
+++ methods/importConfig.js
@@ -1,3 +1,30 @@
+const SuperError = require("../classes/SuperError");
+
 module.exports = (self, config) => {
-    self.config = self.config.concat(config)
-}
+    self.config = self.config.concat(config);
+    if (self.client.user) {
+        config.forEach(message => {
+            let msg = await self.client.channels.cache.get(message.channelID).messages.fetch(message.messageID).catch(err => {
+                throw new SuperError("CanNotFetchMesssage", err.toString());
+            });
+            if (!msg) throw new SuperError("CanNotFetchMesssage", err.toString());
+
+            for (let { emoji } of reactions) {
+                emoji = require("./cleanEmoji")(emoji);
+                let messageReaction = msg.reactions.cache.get(emoji);
+                if (!messageReaction) await msg.react(emoji).catch((err) => {
+                    throw new SuperError("CanNotReactMesssage", err.toString());
+                });
+                else {
+                    if (!messageReaction.me) {
+                        messageReaction.users.fetch();
+                        await msg.react(emoji).catch((err) => {
+                            throw new SuperError("CanNotReactMesssage", err.toString());
+                        });
+                    };
+                };
+            };
+            if (self.mongoURL) self.database.createMessage(message);
+        });
+    };
+};
